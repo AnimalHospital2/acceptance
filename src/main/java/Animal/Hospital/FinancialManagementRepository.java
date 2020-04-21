2ec0215af034a45a826f03349eb9c922b3582668
shkim <saete99@nate.com>
2020-04-21 09:29:09 +0900

    Create FinancialManagementRepository.java

---
+++ src/main/java/Animal/Hospital/FinancialManagementRepository.java
@@ -0,0 +1,8 @@
+package Animal.Hospital;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface FinancialManagementRepository extends PagingAndSortingRepository<FinancialManagement, Long>{
+
+
+}
