1a0fd2910b8f407bc3f4c73e58e9d33337de9045
shkim <saete99@nate.com>
2020-04-21 12:26:19 +0900

    Create Treated.java

---
+++ src/main/java/Animal/Hospital/Treated.java
@@ -0,0 +1,27 @@
+package Animal.Hospital;
+
+public class Treated extends AbstractEvent {
+
+    private Long id;
+    private Long reservationId;
+
+    public Treated(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+	public Long getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(Long reservationId) {
+		this.reservationId = reservationId;
+	}
+}
