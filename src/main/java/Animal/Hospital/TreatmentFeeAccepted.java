c34f45be099afa013c0fff1e483b23ddd068814c
SKCC <SKCC@SKCC18D00072.SKCC.NET>
2020-04-21 13:36:55 +0900

    dev 4.21

--- src/main/java/Animal/Hospital/TreatmentFeeAccepted.java
+++ src/main/java/Animal/Hospital/TreatmentFeeAccepted.java
@@ -5,11 +5,23 @@ public class TreatmentFeeAccepted extends AbstractEvent {
     private Long id;
     private Long reservationId;
     private Long fee;
+    private String eventType;
+
+    @Override
+    public String getEventType() {
+        return eventType;
+    }
+
+    @Override
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
 
     public TreatmentFeeAccepted(Long reservationId, Long fee){
         super();
         this.reservationId = reservationId;
         this.fee = fee;
+        this.eventType  = TreatmentFeeAccepted.class.getSimpleName();
     }
 
     public Long getId() {
@@ -19,4 +31,22 @@ public class TreatmentFeeAccepted extends AbstractEvent {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+
+    public void setFee(Long fee) {
+        this.fee = fee;
+    }
+
+
 }
